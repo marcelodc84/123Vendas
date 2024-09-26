db517539ff3573c2412d3c8cfad46896de7c465a
marcelodc84 <mdc.marcelo@gmail.com>
2024-09-26 17:20:21 -0300

    warning de referencia nula

--- SalesAPI/Domain/Interfaces/ISaleRepository.cs
+++ SalesAPI/Domain/Interfaces/ISaleRepository.cs
@@ -6,7 +6,7 @@ namespace Domain.Interfaces
     public interface ISaleRepository
     {
         Task<IEnumerable<Sale>> GetAllSalesAsync();
-        Task<Sale> GetSaleByIdAsync(int saleNumber);
+        Task<Sale?> GetSaleByIdAsync(int saleNumber);
         Task AddSaleAsync(Sale sale);
         Task UpdateSaleAsync(Sale sale);
         Task DeleteSaleAsync(int saleNumber);
